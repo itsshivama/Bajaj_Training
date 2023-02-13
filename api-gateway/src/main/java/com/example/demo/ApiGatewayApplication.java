7b63137e8accc81f150fb88a1a732881d934ab9f
itsshivama <92708149+itsshivama@users.noreply.github.com>
2023-02-13 14:48:25 +0530

    Eureka Server Sync

---
+++ api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Log4j2
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
